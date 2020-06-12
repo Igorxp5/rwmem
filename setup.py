83e23d78e557e4ea29d636e2e1ca7ae7347246f3
Oseid Aldary <oseid.aldary@gmail.com>
2020-06-12 14:31:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="rwmem",
-    version="0.0.5",
+    version="0.0.6",
     description="A Simple Read/Write Memory module for Windows python",
     long_description=long_description,
     long_description_content_type="text/markdown",
